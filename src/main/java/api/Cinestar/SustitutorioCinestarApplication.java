3e934d0bfc371bde6ddd5cd130f6e21034458f33
PC <PC@SCCTTO71361935>
2023-04-20 21:00:59 -0500

    proyect

---
+++ src/main/java/api/Cinestar/SustitutorioCinestarApplication.java
@@ -0,0 +1,13 @@
+package api.Cinestar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustitutorioCinestarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustitutorioCinestarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/Cinestar/SustitutorioCinestarApplicationTests.java
@@ -0,0 +1,13 @@
+package api.Cinestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustitutorioCinestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
