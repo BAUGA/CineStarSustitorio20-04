e5ffd609c0d6e4edb71ff1bdc6e32057bc686ef0
PC <PC@SCCTTO71361935>
2023-04-20 21:22:00 -0500

    agregando los model

--- src/main/java/api/Cinestar/model/CinePeliculaList.java
+++ src/main/java/api/Cinestar/model/CinePeliculaList.java
@@ -1,5 +1,25 @@
 package api.Cinestar.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+
 public class CinePeliculaList {
+	@Id
+	private String Titulo;
+	private String Horarios;
+	public String getTitulo() {
+		return Titulo;
+	}
+	public void setTitulo(String titulo) {
+		Titulo = titulo;
+	}
+	public String getHorarios() {
+		return Horarios;
+	}
+	public void setHorarios(String horarios) {
+		Horarios = horarios;
+	}
 
 }
--- src/main/java/api/Cinestar/model/PeliculaList.java
+++ src/main/java/api/Cinestar/model/PeliculaList.java
@@ -1,5 +1,41 @@
 package api.Cinestar.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+
 public class PeliculaList {
+	@Id
+	private int id;
+
+	private String Titulo;
+		private String Link;
+	private String Sinopsis;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return Titulo;
+	}
+	public void setTitulo(String titulo) {
+		Titulo = titulo;
+	}
+	public String getLink() {
+		return Link;
+	}
+	public void setLink(String link) {
+		Link = link;
+	}
+	public String getSinopsis() {
+		return Sinopsis;
+	}
+	public void setSinopsis(String sinopsis) {
+		Sinopsis = sinopsis;
+	}
+	
 
 }
