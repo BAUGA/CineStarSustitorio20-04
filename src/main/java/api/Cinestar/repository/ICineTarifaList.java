bbb404f00d4980c1270cf7fd8179207e3b7f6909
PC <PC@SCCTTO71361935>
2023-04-20 21:27:51 -0500

    repositorio

--- src/main/java/api/Cinestar/repository/ICineTarifaList.java
+++ src/main/java/api/Cinestar/repository/ICineTarifaList.java
@@ -1,5 +1,18 @@
 package api.Cinestar.repository;
 
-public interface ICineTarifaList {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import api.Cinestar.model.CineTarifaList;
+
+@Repository
+
+public interface ICineTarifaList   extends JpaRepository<CineTarifaList, Integer> {
+	@Query(value = "{call sp_getCineTarifas(:id)}", nativeQuery= true)
+	List<CineTarifaList> getCineTarifaList(int id);
+
 
 }
