cf7a0ef6e974108e33f31390b690eb497cfbf75d
PC <PC@SCCTTO71361935>
2023-04-20 21:28:24 -0500

    repositorio

--- src/main/java/api/Cinestar/repository/IPelicula.java
+++ src/main/java/api/Cinestar/repository/IPelicula.java
@@ -1,5 +1,16 @@
 package api.Cinestar.repository;
 
-public interface IPelicula {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import api.Cinestar.model.Pelicula;
+
+@Repository
+
+public interface IPelicula extends JpaRepository<Pelicula, Integer>{
+	
+	@Query(value ="{call sp_getPelicula(:id)}", nativeQuery = true)
+	Pelicula getPelicula(int id);
 
 }
