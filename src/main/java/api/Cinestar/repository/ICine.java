ad0fb9874b5b216addf0920af40ccfc8cc087dbc
PC <PC@SCCTTO71361935>
2023-04-20 21:25:43 -0500

    repositorio

--- src/main/java/api/Cinestar/repository/ICine.java
+++ src/main/java/api/Cinestar/repository/ICine.java
@@ -1,5 +1,15 @@
 package api.Cinestar.repository;
 
-public interface ICine {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import api.Cinestar.model.Cine;
+
+@Repository
+
+public interface ICine extends JpaRepository<Cine, Integer> {
+	@Query (value = "{call sp_getCine(:id)}", nativeQuery = true)
+	Cine getCine(int id);
 
 }
