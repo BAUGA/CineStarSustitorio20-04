d5fc638daca4680d0fa5511166685986c1be2ead
PC <PC@SCCTTO71361935>
2023-04-20 21:26:46 -0500

    repositorio

--- src/main/java/api/Cinestar/repository/ICineList.java
+++ src/main/java/api/Cinestar/repository/ICineList.java
@@ -1,5 +1,19 @@
 package api.Cinestar.repository;
 
-public interface ICineList {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import api.Cinestar.model.CineList;
+
+@Repository
+
+
+public interface ICineList   extends JpaRepository<CineList, Integer> {
+	@Query(value ="{call sp_getCines()}", nativeQuery= true)
+	List<CineList> getCines();
+	
 
 }
